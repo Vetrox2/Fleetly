39af14887f0124bb98142d64d5510e9f9d5e255e
Vetrox2 <mateuszmulka@spoko.pl>
2025-04-29 14:41:11 +0200

    init ASP.NET Core MVC

---
+++ Fleetly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fleetly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
