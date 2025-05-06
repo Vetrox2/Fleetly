1d8b23a5a10fcf85b8a30808cd15e21f854f452d
Daniel Dwornik <44972248+CptDaniels@users.noreply.github.com>
2025-05-06 10:58:57 +0200

    feat(controller): init controllers

---
+++ Fleetly/Controllers/RouteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fleetly.Controllers
+{
+    public class RouteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
